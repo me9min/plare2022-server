b1a845e4cad3125de4e492781d88f7c686431499
me9min <zlws554@gmail.com>
2022-10-02 00:11:55 +0900

    상점품목올리기 실험2
    #2 상점품목 테이블 구조대로 VO,매퍼 생성

--- src/main/java/org/amel/plare/store/domain/StoreMenuVO.java
+++ src/main/java/org/amel/plare/store/domain/StoreMenuVO.java
@@ -5,4 +5,19 @@ import lombok.Data;
 @Data
 public class StoreMenuVO {
     
+    private int id;
+    
+    private int groupId;
+    
+    private int typeId;
+    
+    private int price;
+    
+    private String name;
+    
+    private String flag;
+    
+    private String attributeJson;
+    
+    private String description;
 }
