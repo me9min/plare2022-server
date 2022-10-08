46b8346f3f82040d45ce460bb9c3639f32eb389a
me9min <zlws554@gmail.com>
2022-10-08 18:08:31 +0900

    :add crud완성

--- src/main/java/org/amel/plare/board/notice/controller/BoardNoticeController.java
+++ src/main/java/org/amel/plare/board/notice/controller/BoardNoticeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.amel.plare.board.notice.domain.BoardNoticeVO;
 import org.amel.plare.board.notice.service.BoardNoticeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +21,17 @@ public class BoardNoticeController {
     public List<BoardNoticeVO> listBoardNotice() {
         
         return boardNoticeService.listBoardNotice();
+    }
+    
+    @RequestMapping(value = "/notice", method = RequestMethod.POST)
+    public String insertBoardNotice(@RequestBody BoardNoticeVO boardNotice) {
+        
+        return "insert된 엔티티수: " + Integer.toString(boardNoticeService.insertBoardNotice(boardNotice));
+    }
+    
+    @RequestMapping(value = "/notice", method = RequestMethod.PATCH)
+    public String updateBoardNotice(@RequestBody BoardNoticeVO boardNotice) {
+        
+        return "update된 엔티티수: " + Integer.toString(boardNoticeService.updateBoardNotice(boardNotice));
     }
 }
--- src/main/java/org/amel/plare/board/notice/service/BoardNoticeService.java
+++ src/main/java/org/amel/plare/board/notice/service/BoardNoticeService.java
@@ -6,6 +6,7 @@ import org.amel.plare.board.notice.dao.BoardNoticeDao;
 import org.amel.plare.board.notice.domain.BoardNoticeVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class BoardNoticeService {
@@ -16,5 +17,20 @@ public class BoardNoticeService {
     public List<BoardNoticeVO> listBoardNotice() {
         
         return boardNoticeDao.listBoardNotice();
+    }
+    
+    public int insertBoardNotice(@RequestBody BoardNoticeVO boardNotice) {
+        
+        boardNotice.setIpOfCreated("127.0.0.1");
+        boardNotice.setIpOfUpdated("127.0.0.1");
+        
+        return boardNoticeDao.insertBoardNotice(boardNotice);
+    }
+    
+    public int updateBoardNotice(@RequestBody BoardNoticeVO boardNotice) {
+        
+        boardNotice.setIpOfUpdated("127.0.0.1");
+        
+        return boardNoticeDao.updateBoardNotice(boardNotice);
     }
 }
